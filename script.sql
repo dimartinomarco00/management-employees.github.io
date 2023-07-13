3a6f18602fd9adc3b51a8f19b4dcb69335e4260b
Di Martino Marco <95447687+dimartinomarco00@users.noreply.github.com>
2023-07-13 22:31:02 +0200

    script.sql

--- script.sql
+++ script.sql
@@ -4,9 +4,14 @@ CREATE TABLE `dipendenti` (
   `cognome` varchar(45) NOT NULL,
   `eta` varchar(45) NOT NULL,
   `sesso` varchar(45) NOT NULL,
+  `nazionalita` varchar(100) DEFAULT NULL,
+  `codice_fiscale` varchar(16) NOT NULL,
   `data_nascita` date NOT NULL,
+  `luogo_nascita` varchar(100) NOT NULL,
+  `luogo_residenza` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `numero_telefono` varchar(45) NOT NULL,
   `salario` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `codice_fiscale` (`codice_fiscale`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
